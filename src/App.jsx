0f448a7634928ea2cdbb77762f758fd65f4e6ea6
Mahnoor-Anwar <mahnoordev@gmail.com>
2024-09-07 16:33:50 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -39,32 +39,31 @@ function App() {
       <Route index element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       </Route>
-    </Routes>
-      <Routes>
-        <Route element={<ProtectedRoute />}>
+      <Route element={<ProtectedRoute />}>
       
-        <Route path="/dashboard" element={<Home />} />
-        <Route path='/studentList' element={<StudentList />} />
-        <Route path='/addStudent' element={<AddStudent />} />
-        <Route path='/editStudent/:id' element={<EditStudent />} />
-        <Route path='/teacherList' element={<TeacherList />} />
-        <Route path='/addTeacher' element={<AddTeacher />} />
-        <Route path='/editTeacher/:id' element={<EditTeacher />} />
-        <Route path='/admissionForm' element={<AdmissionForm />} />
-        <Route path='/admissionList' element={<ClassList />} />
-        <Route path='/editClass/:id' element={<EditClass />} />
-        <Route path='/feeStructure' element={<FeeStructure />} />
-        <Route path='/feeVoucher' element={<FeeVoucher />} />
-        <Route path='/subjectList' element={<SubjectList />} />
-        <Route path='/addSubject' element={<AddSubject />} />
-        <Route path='/syllabusList' element={<SyllabusList />} />
-        <Route path='/editSubject/:id' element={<EditSubject />} />
-        <Route path='/addSyllabus' element={<AddSyllabus />} />
-        <Route path='/editSyllabus/:id' element={<EditSyllabus />} />
-        <Route path='/examSchedule' element={<ExamSchedule />} />
-        <Route path='/CreatePaper' element={<CreatePaper />} />
-        </Route>
-      </Routes>
+      <Route path="/dashboard" element={<Home />} />
+      <Route path='/studentList' element={<StudentList />} />
+      <Route path='/addStudent' element={<AddStudent />} />
+      <Route path='/editStudent/:id' element={<EditStudent />} />
+      <Route path='/teacherList' element={<TeacherList />} />
+      <Route path='/addTeacher' element={<AddTeacher />} />
+      <Route path='/editTeacher/:id' element={<EditTeacher />} />
+      <Route path='/admissionForm' element={<AdmissionForm />} />
+      <Route path='/admissionList' element={<ClassList />} />
+      <Route path='/editClass/:id' element={<EditClass />} />
+      <Route path='/feeStructure' element={<FeeStructure />} />
+      <Route path='/feeVoucher' element={<FeeVoucher />} />
+      <Route path='/subjectList' element={<SubjectList />} />
+      <Route path='/addSubject' element={<AddSubject />} />
+      <Route path='/syllabusList' element={<SyllabusList />} />
+      <Route path='/editSubject/:id' element={<EditSubject />} />
+      <Route path='/addSyllabus' element={<AddSyllabus />} />
+      <Route path='/editSyllabus/:id' element={<EditSyllabus />} />
+      <Route path='/examSchedule' element={<ExamSchedule />} />
+      <Route path='/CreatePaper' element={<CreatePaper />} />
+      </Route>
+    </Routes>
+      
     </>
   )
 }
